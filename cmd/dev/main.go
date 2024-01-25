fde09844b5ab98326620411b56b3e7c3821b7db8
oneofthezombies <hunhoekim@gmail.com>
2024-01-25 19:45:58 +0900

    wip: dev cli

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -1,41 +1,15 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"fmt"
 	"os"
-	"os/exec"
-)
 
-const (
-	typescriptPath = "third_party/typescript"
+	"github.com/oneofthezombies/stariler/cmd/dev/cmd"
 )
 
 func main() {
-	initFlag := flag.Bool("init", false, "initialize development environment")
-	flag.Parse()
-
-	switch {
-	case *initFlag:
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		if _, err := os.Stat(typescriptPath); err == nil {
-			fmt.Printf("%s already exists. deleting...\n", typescriptPath)
-			if err := os.RemoveAll(typescriptPath); err != nil {
-				panic(err)
-			}
-		}
-
-		cmd := exec.CommandContext(ctx, "git", "clone", "--depth", "1", "--branch", "v5.3.3", "https://github.com/oneofthezombies/TypeScript.git", typescriptPath)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			panic(err)
-		}
-	default:
-		flag.Usage()
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
