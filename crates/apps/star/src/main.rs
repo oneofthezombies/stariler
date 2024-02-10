3f50f1bfc7286aae9f56fafd3843e61a36a77460
oneofthezombies <hunhoekim@gmail.com>
2024-02-11 04:12:36 +0900

    wip: star

--- crates/apps/star/src/main.rs
+++ crates/apps/star/src/main.rs
@@ -1,3 +1,28 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(arg_required_else_help = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {}
+
+fn init_log() {
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_max_level(tracing::Level::TRACE)
+            .finish(),
+    )
+    .expect("setting default subscriber failed");
+}
+
 fn main() {
-    println!("Hello, world!");
+    init_log();
+    let cli = Cli::parse();
+    let Some(_command) = cli.command else {
+        panic!("No command");
+    };
 }
