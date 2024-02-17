f604667e7184a317340ac4039b049e1821df08ee
oneofthezombies <hunhoekim@gmail.com>
2024-02-17 21:54:21 +0900

    chore: apply clippy

--- crates/libs/stariler/src/tsconfig.rs
+++ crates/libs/stariler/src/tsconfig.rs
@@ -31,7 +31,7 @@ pub(crate) fn update_exclude(ts_config: TsConfig) -> TsConfig {
     if ts_config.exclude.is_some() {
         return ts_config;
     }
-    let mut exclude: Vec<String> = DEFAULT_EXCLUDE.iter().map(|s| s.to_string()).collect();
+    let mut exclude: Vec<String> = DEFAULT_EXCLUDE.iter().map(|s| (*s).to_string()).collect();
     if let Some(compiler_options) = &ts_config.compiler_options {
         if let Some(out_dir) = &compiler_options.out_dir {
             exclude.push(out_dir.clone());
